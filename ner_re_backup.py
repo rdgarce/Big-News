355309237871cf9d12ac4ca5d45a0ba357425235
davidev9 <vitaledavideu@gmail.com>
2024-06-09 08:21:16 +0200

    bug fixes

--- ner_re_backup.py
+++ ner_re_backup.py
@@ -245,27 +245,27 @@ def process_articles(articles,max_attempts=2,chunk_size=1200,last_check=True):
     for j, article in enumerate(tqdm(articles, desc="Processing Articles")):
         lunghezza_text = len(article["text"])
         print("La lunghezza dell'articolo", j+1, "è:", lunghezza_text)
-        torch.cuda.empty_cache()
-        gc.collect()
+        #torch.cuda.empty_cache()
+        #gc.collect()
         max_attempts = 2
         attempts = 0
 
-        if lunghezza_text < 3000:
-            chunks = chunk_article(article, chunk_size)
-            for i, chunk in enumerate(tqdm(chunks, desc=f"Processing Chunks for Article {j+1}", leave=False)):
-                triple_chunk = []
-                attempts = 0
-                while attempts < max_attempts and not triple_chunk:
-                    triple_chunk = estrai_triple(chunk['text'], chunk['date'], chunk['link'],last_check)
-                    attempts += 1
-                if triple_chunk:
-                    print(f"Triple estratte dal Chunk {i+1} dell'articolo {j+1}")
-                for istance in triple_chunk:
-                    table_data.append([istance.entity1, istance.category1, istance.relation, istance.sentiment, istance.entity2, istance.category2, istance.data, istance.link])
-                    torch.cuda.empty_cache()
-                print(tabulate(table_data, headers="firstrow", tablefmt="grid"))
-                triple_totali += triple_chunk
+        
+        chunks = chunk_article(article, chunk_size)
+        for i, chunk in enumerate(tqdm(chunks, desc=f"Processing Chunks for Article {j+1}", leave=False)):
+            triple_chunk = []
+            attempts = 0
+            while attempts < max_attempts and not triple_chunk:
+                triple_chunk = estrai_triple(chunk['text'], chunk['date'], chunk['link'],last_check)
+                attempts += 1
+            if triple_chunk:
+                print(f"Triple estratte dal Chunk {i+1} dell'articolo {j+1}")
+            for istance in triple_chunk:
+                table_data.append([istance.entity1, istance.category1, istance.relation, istance.sentiment, istance.entity2, istance.category2, istance.data, istance.link])
                 torch.cuda.empty_cache()
+            print(tabulate(table_data, headers="firstrow", tablefmt="grid"))
+            triple_totali += triple_chunk
+            torch.cuda.empty_cache()
 
     return triple_totali
 
